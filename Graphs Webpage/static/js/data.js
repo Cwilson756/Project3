3b29480990d76b56314786e27dc757fbd4e96632
richardmorenorm <115674964+richardmorenorm@users.noreply.github.com>
2023-04-17 21:44:12 -0500

    yearly US change graph

--- Graphs Webpage/static/js/data.js
+++ Graphs Webpage/static/js/data.js
@@ -1,3 +1,5 @@
+var arrayName = [];
+
 d3.csv("data/monthlyStateValue.csv").then((importedData) => {
   // console.log(importedData);
   let MonthlyStateValue = importedData;
@@ -12,15 +14,28 @@ d3.csv("data/yearlyCountyIncome.csv").then((importedData) => {
 });
 d3.csv("data/yearlyStateValue.csv").then((importedData) => {
   // console.log(importedData);
-  let YearlyStateValue = importedData;
+  var YearlyStateValue = importedData;
 
   console.log(YearlyStateValue);
 });
+var fred = [];
+
 d3.csv("data/yearlyUSValue.csv").then((importedData) => {
   // console.log(importedData);
   let YearlyUSValue = importedData;
-
-  console.log(YearlyUSValue);
+  let listList = YearlyUSValue.map(dict => Object.values(dict));
+  let numberList = listList.map(innerList => innerList.map(str => parseFloat(str)));
+  var dataSet = anychart.data.set(numberList);
+  var seriesData = dataSet.mapAs({ x: 0, value: 1 });
+  var chart = anychart.line();
+  var lineChart = chart.line(seriesData);
+  // set the container id for the line chart
+  chart.container('container');
+  // draw the line chart
+  chart.draw();
+  console.log(numberList);
+  //console.log(YearlyUSValue);
+  //fred = YearlyUSValue
 });
 d3.csv("data/CensusData.csv").then((importedData) => {
   // console.log(importedData);
@@ -28,3 +43,32 @@ d3.csv("data/CensusData.csv").then((importedData) => {
 
   console.log(CensusData);
 });
+
+/*console.log(YearlyUSValue.length);
+let arr = [];
+for (let i = 0; i < YearlyUSValue.length; i++) {
+  arr.push(YearlyUSValue[i]);
+}
+console.log(arr);
+var dict = YearlyUSValue;
+var arr = [];
+
+for (var key in dict) {
+    if (dict.hasOwnProperty(key)) {
+        arr.push( [ key, dict[key] ] );
+    }
+}
+console.log(arr);
+
+
+var dataSet = anychart.data.set(listList());
+var seriesData = dataSet.mapAs({ x: 0, value: 1 });
+var chart = anychart.line();
+var lineChart = chart.line(seriesData);
+
+// set the container id for the line chart
+chart.container('container');
+
+// draw the line chart
+chart.draw();
+*/
