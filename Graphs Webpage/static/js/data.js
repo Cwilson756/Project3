d79a3cf21856805f4b2e33068393dfd6f4ee62e3
AwesomeAggie <woodulscollege@yahoo.com>
2023-04-18 21:31:51 -0500

    line plot owrking!

--- Graphs Webpage/static/js/data.js
+++ Graphs Webpage/static/js/data.js
@@ -1,75 +1,62 @@
-function getData (){
-  d3.csv("data/yearlyUSValue.csv").then((importedData) => {
-    // console.log(importedData);
-    let YearlyUSValue = importedData;
-    let USlistList = YearlyUSValue.map(dict => Object.values(dict));
-    let USnumberList = USlistList.map(innerList => innerList.map(str => parseFloat(str)));
-    console.log(USnumberList);
-  });
-  d3.csv("data/CensusData.csv").then((importedData) => {
-    // console.log(importedData);
-    let CensusData = importedData;
-    let CensuslistList = CensusData.map(dict => Object.values(dict));
-    let CensusnumberList = CensuslistList.map(innerList => innerList.map(str => parseFloat(str)));
-    console.log(CensusnumberList);
-  });
-  d3.csv("data/yearlyStateValue.csv").then((importedData) => {
-  // console.log(importedData);
-    let YearlyStateValue = importedData;
-    let StatelistList = YearlyStateValue.map(dict => Object.values(dict));
-    let StatenumberList = StatelistList.map(innerList => innerList.map(str => parseFloat(str)));
-    console.log(StatenumberList);
-  });
-  }
-  function GetData(USnumberList){
-    USArray = [];
-    for (let i = 0; i < USnumberList.length; i++){
-      USArray.push(USnumberList[i]);
-  
-    }
-    //call somecharting functin
-    ChartIt(USArray);
-  
-  };
-function ChartIt(the_data_you_need_for_charting){
-    console.log("This One");
-    console.log(the_data_you_need_for_charting);
-      var dataSet = anychart.data.set(the_data_you_need_for_charting);
-      var seriesData = dataSet.mapAs({ x: 0, value: 1 });
-      var chart = anychart.line();
-      var lineChart = chart.line(seriesData);
-      // set the container id for the line chart
-      chart.container('container');
-      // draw the line chart
-      chart.draw();
-    }
-getData();
+let States = "data/yearlyStateValue1.json"
 
-/*console.log(YearlyUSValue.length);
-let arr = [];
-for (let i = 0; i < YearlyUSValue.length; i++) {
-  arr.push(YearlyUSValue[i]);
+Statesdata=d3.json(States);
+
+function init() {
+
+    // dropdown menu
+    let dropdownMenu = d3.select("#selDataset");
+
+    // grab the data
+    Statesdata.then((data) => {
+
+        let names = data.names;
+        names.forEach((name) => {
+            dropdownMenu.append("option").text(name).property("value", name);
+        });
+
+        let name = names[0];
+
+        // calls the line function
+        line(name);
+    });
 }
-console.log(arr);
-var dict = YearlyUSValue;
-var arr = [];
 
-for (var key in dict) {
-    if (dict.hasOwnProperty(key)) {
-        arr.push( [ key, dict[key] ] );
-    }
+
+function line(selection) {
+    Statesdata.then((data) => {
+
+        let states = data.states;
+        let filteredData = states.filter((state) => state.RegionName === selection);
+        let obj = filteredData[0];
+        var dict = obj;
+        var x_arr = [];
+        var y_arr = [];
+
+        for (var key in dict) {
+            if (dict.hasOwnProperty(key)) {
+                console.log(key);
+                console.log(dict[key]);
+            x_arr.push( key );
+            y_arr.push(dict[key] );
+            }
+        }
+        var trace1 = {
+        x: x_arr,
+        y: y_arr,
+        type: 'scatter'
+    };
+  
+    var data = [trace1];
+  
+    Plotly.newPlot('myDiv', data);
+
+});
 }
-console.log(arr);
 
+function optionChanged(selection) {
+    line(selection);
+}
 
-var dataSet = anychart.data.set(listList());
-var seriesData = dataSet.mapAs({ x: 0, value: 1 });
-var chart = anychart.line();
-var lineChart = chart.line(seriesData);
+init();
 
-// set the container id for the line chart
-chart.container('container');
-
-// draw the line chart
-chart.draw();
-*/
