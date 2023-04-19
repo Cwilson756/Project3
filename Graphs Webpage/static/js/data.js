e9240f61797e158acca6778c2a7af978ec7d37f9
AwesomeAggie <woodulscollege@yahoo.com>
2023-04-18 22:54:38 -0500

    line plot finished!

--- Graphs Webpage/static/js/data.js
+++ Graphs Webpage/static/js/data.js
@@ -1,6 +1,8 @@
 let States = "data/yearlyStateValue1.json"
-
 Statesdata=d3.json(States);
+
+let Country = "data/yearlyUSValue.json"
+Counrtydata=d3.json(Country);
 
 function init() {
 
@@ -38,18 +40,41 @@ function line(selection) {
                 console.log(key);
                 console.log(dict[key]);
             x_arr.push( key );
-            y_arr.push(dict[key] );
+            y_arr.push(dict[key]);
             }
         }
+
+        var x_arr1 = [2000,2001,2002,2003,2004,2005,2006,2007,2008,2009,2010,2011,
+                      2012,2013,2014,2015,2016,2017,2018,2019,2020,2021,2022];
+        var y_arr1 = [167550,173100,186025,192125,218150,236550,243750,244950,229550,215650,222700,224900,244400,266225,285775,
+                      294150,305125,322425,325275,320250,336950,396800,454525];
+
+
         var trace1 = {
         x: x_arr,
         y: y_arr,
-        type: 'scatter'
+        mode: 'lines+markers',
+        type: 'scatter',
+        name: selection
+    };
+
+        var trace2 = {
+        x: x_arr1,
+        y: y_arr1,
+        mode: 'lines+markers',
+        type: 'scatter',
+        name: "National Average"
     };
   
-    var data = [trace1];
+    var data = [trace1, trace2];
+
+    var layout = {
+        title:"Average Housing Prices Per Year",
+        xaxis: {title: "Year"},
+        yaxis: {title: "Average Housing Price"}
+    }
   
-    Plotly.newPlot('myDiv', data);
+    Plotly.newPlot('myDiv', data, layout);
 
 });
 }
